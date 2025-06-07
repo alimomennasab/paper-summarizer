30420ed5a3e46538de0b7f04d2a01ffa5ba5e659
alimomennasab <alimomennasab3@gmail.com>
2025-06-07 16:34:10 -0700

    frontend init

---
+++ frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="bg-[var(--foreground)] text-[var(--background)] p-4 font-extrabold text-2xl">
+      Paper Summarizer
+    </div>
+  );
+}
---
+++ frontend/src/app/components/UploadArea.tsx
@@ -0,0 +1,10 @@
+export default function UploadArea() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <button className="mt-4 bg-[var(--foreground)] text-[var(--background)] px-4 py-2 rounded hover:bg-[var(--foreground-hover)] transition-colors duration-300">
+        Upload Your Paper Here
+      </button>
+    </div>
+  );
+}
+
