ecb6913287eba7e5cb3b3594548c6541df6e58ee
alimomennasab <alimomennasab3@gmail.com>
2025-06-07 20:18:11 -0700

    homepage ui update

--- frontend/src/app/components/UploadArea.tsx
+++ frontend/src/app/components/UploadArea.tsx
@@ -1,8 +1,8 @@
 export default function UploadArea() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <button className="mt-4 bg-[var(--foreground)] text-[var(--background)] px-4 py-2 rounded hover:bg-[var(--foreground-hover)] transition-colors duration-300">
-        Upload Your Paper Here
+    <div className="flex flex-col items-center justify-center h-1/2 w-1/2">
+      <button className="p-10 bg-[var(--foreground)] text-[var(--background)] font-bold text-lg px-4 py-2 rounded hover:bg-[var(--foreground-hover)] transition-colors duration-200">
+        Upload Your Paper
       </button>
     </div>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,28 @@ import UploadArea from './components/UploadArea';
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-screen w-screen">
       <Navbar />
-      <UploadArea />
+      
+      <div className='flex flex-1 flex-col items-center justify-center w-full max-w-xl mx-auto px-6'> 
+        <div className="flex flex-col items-center justify-center border border-[var(--border-color)] rounded-lg gap-15 p-10 shadow-md">
+          <h1 className="text-4xl font-bold text-center">
+            Summarize research papers.
+          </h1>
+          <p className="text-lg text-center text-[var(--dark)]">
+            Tired of long, confusing papers? Want to get the gist quickly?
+            Upload your research paper as a PDF and get a concise summary in seconds.
+          </p>
+          
+          <UploadArea />
+        </div>
+      </div>
+
+      <footer className="bg-[var(--foreground)] text-[var(--background)] p-2 text-center">
+        <p className="text-sm italic">
+          Made with ❤️ by Ali
+        </p>
+      </footer>
     </div>
   );
 }
