a2ea384e4fa677c7e2b69d31677475cae20463c6
alimomennasab <alimomennasab3@gmail.com>
2025-06-10 16:47:07 -0700

    redirect to home with navbar

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+
 export default function Navbar() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/');
+  };
+
   return (
     <div className="bg-[var(--foreground)] text-[var(--background)] p-4 font-extrabold text-2xl">
-      Paper Summarizer
+      <button 
+        className="cursor-pointer"
+        onClick={handleClick}
+      >
+        Paper Summarizer
+      </button>
     </div>
   );
 }
