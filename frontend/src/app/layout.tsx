700c7a1c5d9f3f980d75a0ede9aabcdc9d02bd90
alimomennasab <alimomennasab3@gmail.com>
2025-06-12 23:02:23 -0700

    updated favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Research Paper Summarizer",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
