77fa0cbf9703fa139fb723f88f40f6a2769326b8
alimomennasab <alimomennasab3@gmail.com>
2025-06-12 23:06:21 -0700

    updated footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -2,7 +2,14 @@ export default function Footer() {
     return (
         <footer className="bg-[var(--foreground)] text-[var(--background)] p-2 text-center">
             <p className="text-sm italic">
-                Made with ❤️ by Ali
+                Made with ❤️ by 
+                <a 
+                href="https://github.com/alimomennasab" 
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline">
+                    {' '}Ali Momennasab
+                </a>
             </p>
         </footer>
     )
